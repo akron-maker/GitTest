b8aba5ac06167605ee08fd13751b27bbecc000d7
akron <1511553138@qq.com>
2022-09-12 14:32:09 +0800

    添加controller

--- src/main/java/com/akron/gittest/controller/GitController.java
+++ src/main/java/com/akron/gittest/controller/GitController.java
@@ -1,5 +1,6 @@
 package com.akron.gittest.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,4 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class GitController {
+
+    @RequestMapping("/git")
+    public String test1(){
+        return "gittest!!!";
+    }
 }
