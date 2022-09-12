28d65d7b202096d85d7084aa11928132c260be50
akron <1511553138@qq.com>
2022-09-12 14:22:56 +0800

    第一次提交

---
+++ src/main/java/com/akron/gittest/controller/GitController.java
@@ -0,0 +1,12 @@
+package com.akron.gittest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author akron
+ * @version 1.0
+ * @date 2022/9/12 14:21
+ */
+@RestController
+public class GitController {
+}
---
+++ src/main/java/com/akron/gittest/domain/Student.java
@@ -0,0 +1,15 @@
+package com.akron.gittest.domain;
+
+import lombok.Data;
+
+/**
+ * @author akron
+ * @version 1.0
+ * @date 2022/9/12 14:15
+ */
+@Data
+public class Student {
+    private int id;
+    private String name;
+    private String address;
+}
